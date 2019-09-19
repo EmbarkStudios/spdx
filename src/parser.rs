9333a6037bb99ba71b5838d22cf3315fa8fb18be
Jake Shadle <jake.shadle@embark-studios.com>
2019-09-19 12:10:10 +0200

    Revamp parser for better errors

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 use crate::{
-    exception_id,
+    error::{ParseError, Reason},
     lexer::{Lexer, Token},
-    license_id, LicenseItem, LicenseReq, ParseError,
+    LicenseItem, LicenseReq,
 };
 use smallvec::SmallVec;
 use std::fmt;
@@ -15,12 +15,14 @@ enum ExprNode<'a> {
 /// An SPDX license expression that is both syntactically
 /// and semantically valid, and can be evaluated
 pub struct ValidExpression<'a> {
-    expr: SmallVec<[ExprNode<'a>; 3]>,
+    expr: SmallVec<[ExprNode<'a>; 5]>,
     // We keep the original string around for display purposes only
     original: &'a str,
 }
 
 impl<'a> ValidExpression<'a> {
+    /// Returns each of the license requirements in the license expression,
+    /// but not the operators that join them together
     pub fn licenses(&self) -> impl Iterator<Item = &LicenseReq<'a>> {
         self.expr.iter().filter_map(|item| match item {
             ExprNode::Req(req) => Some(req),
@@ -99,7 +101,7 @@ impl<'a> fmt::Display for ValidExpression<'a> {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
 pub enum Operator {
     And,
     Or,
@@ -116,221 +118,217 @@ impl<'a> ValidExpression<'a> {
     /// * A license or exception immediately follows another license or exception, without
     /// a valid AND, OR, or WITH operator separating them
     /// * An AND, OR, or WITH doesn't have a license or `)` preceding it
-    pub fn parse(expr: &'a str) -> Result<Self, ParseError> {
-        let lexer = Lexer::new(expr);
+    pub fn parse(original: &'a str) -> Result<Self, ParseError> {
+        let lexer = Lexer::new(original);
 
-        // Operator precedence as of SPDX 2.1
+        // Operator precedence in SPDX 2.1
         // +
         // WITH
         // AND
         // OR
-        #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+        #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
         enum Op {
-            With,
+            //Plus,
+            //With,
             And,
             Or,
-            Open(usize),
+            Open,
         }
 
-        let mut op_stack = SmallVec::<[Op; 3]>::new();
-        let mut expr_queue = SmallVec::<[ExprNode; 3]>::new();
+        struct OpAndSpan {
+            op: Op,
+            span: std::ops::Range<usize>,
+        }
 
-        'outer: for tok in lexer {
-            let lex_tok = tok?;
-            let cur_str = &expr[lex_tok.start..lex_tok.end];
+        let mut op_stack = SmallVec::<[OpAndSpan; 3]>::new();
+        let mut expr_queue = SmallVec::<[ExprNode; 5]>::new();
 
-            let license_item = match lex_tok.token {
-                Token::License(lic) => LicenseItem::SPDX(
-                    license_id(lic).ok_or_else(|| ParseError::UnknownLicenseId(lic))?,
-                ),
-                Token::LicenseRef { doc, lic } => LicenseItem::Other {
-                    document_ref: doc,
-                    license_ref: lic,
-                },
-                Token::Plus => {
-                    // Anything other than an open parens at the top of the op stack means
-                    // the plus is following another operator, which isn't allowed
-                    match op_stack.last() {
-                        Some(Op::Open(_)) | None => {}
-                        _ => return Err(ParseError::UnexpectedToken(cur_str, &["<license>"])),
-                    }
+        // Keep track of the last token to simplify validation of the token stream
+        let mut last_token: Option<Token<'_>> = None;
 
-                    match expr_queue.last_mut() {
-                        Some(ExprNode::Req(lic)) => {
-                            // This isn't C++, the + is only an unary suffix
-                            if lic.or_later {
-                                return Err(ParseError::UnexpectedToken(
-                                    cur_str,
-                                    &["AND", "OR", "WITH"],
-                                ));
-                            } else {
-                                lic.or_later = true;
-                            }
-                        }
-                        _ => return Err(ParseError::UnexpectedToken(cur_str, &["<license>"])),
-                    }
-
-                    continue;
-                }
-                op_tok @ Token::Or | op_tok @ Token::And => {
-                    // while ((there is a function at the top of the operator stack)
-                    //     or (there is an operator at the top of the operator stack with greater precedence)
-                    //     or (the operator at the top of the operator stack has equal precedence and is left associative))
-                    //     and (the operator at the top of the operator stack is not a left parenthesis):
-                    //     pop operators from the operator stack onto the output queue.
-                    // push it onto the operator stack.
-
-                    let new_op = match op_tok {
-                        Token::Or => Op::Or,
-                        Token::And => Op::And,
-                        _ => unreachable!(),
-                    };
-
-                    while let Some(op) = op_stack.last() {
-                        match op {
-                            Op::Open(_) => break,
-                            other => {
-                                if *other < new_op {
-                                    match op_stack.pop().unwrap() {
-                                        Op::And => expr_queue.push(ExprNode::Op(Operator::And)),
-                                        Op::Or => expr_queue.push(ExprNode::Op(Operator::Or)),
-                                        _other => {
-                                            return Err(ParseError::UnexpectedToken(
-                                                cur_str,
-                                                &["<license>"],
-                                            ))
-                                        }
-                                    }
-                                } else {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-
-                    // match op_stack.last() {
-                    //     Some(Op::Or) => {
-                    //         if new_op == Op::And {
-                    //             //println!("TOP IS {:?} @ INDEX {}", top, op_stack.len() - 1);
-                    //             op_stack.insert(op_stack.len() - 2, new_op);
-                    //             continue;
-                    //         }
-                    //     }
-                    //     _ => {}
-                    // }
-
-                    op_stack.push(new_op);
-                    continue;
-                }
-                Token::With => {
-                    // WITH must directly follow a license
-                    // match op_stack.last() {
-                    //     Some(Op::Open(_)) | None => {},
-                    //     _ => return Err(ParseError::UnexpectedToken(cur_str, &["<license>"])),
-                    // }
-
-                    match expr_queue.last() {
-                        Some(ExprNode::Req(lr)) => {
-                            if lr.exception.is_some() {
-                                return Err(ParseError::UnexpectedToken(cur_str, &["AND", "OR"]));
-                            }
-                        }
-                        _ => return Err(ParseError::UnexpectedToken(cur_str, &["<license>"])),
-                    }
-
-                    op_stack.push(Op::With);
-                    continue;
-                }
-                Token::OpenParen => {
-                    op_stack.push(Op::Open(lex_tok.start));
-                    continue;
-                }
-                Token::CloseParen => {
-                    while let Some(op) = op_stack.pop() {
-                        match op {
-                            Op::Or => expr_queue.push(ExprNode::Op(Operator::Or)),
-                            Op::And => expr_queue.push(ExprNode::Op(Operator::And)),
-                            Op::Open(_) => continue 'outer,
-                            Op::With => {
-                                return Err(ParseError::UnexpectedToken(cur_str, &["<exception>"]));
-                            }
-                        }
-                    }
-
-                    // We didn't have an opening parentheses if we get here
-                    return Err(ParseError::UnbalancedParen(lex_tok.start));
-                }
-                Token::Exception(exc) => {
-                    // Exceptions can only follow a WITH, and a WITH can only
-                    // follow a license, so we just pop the WITH op off and
-                    // directly modify the preceding license
-                    match op_stack.last() {
-                        Some(Op::With) => {
-                            op_stack.pop();
-
-                            let exc_id = exception_id(exc)
-                                .ok_or_else(|| ParseError::UnknownExceptionId(exc))?;
-
-                            match expr_queue.last_mut() {
-                                Some(ExprNode::Req(lr)) => {
-                                    lr.exception = Some(exc_id);
-                                }
-                                _ => {
-                                    return Err(ParseError::UnexpectedToken(
-                                        cur_str,
-                                        &["<license>"],
-                                    ))
-                                }
-                            }
-                        }
-                        _ => return Err(ParseError::UnexpectedToken(cur_str, &["<license>"])),
-                    }
-
-                    continue;
-                }
+        let apply_op = |op: OpAndSpan, q: &mut SmallVec<[ExprNode<'_>; 5]>| {
+            let op = match op.op {
+                Op::And => Operator::And,
+                Op::Or => Operator::Or,
+                _ => unreachable!(),
             };
 
-            // Ensure we have a valid preceding token
-            if let Some(Op::With) = op_stack.last() {
-                return Err(ParseError::UnexpectedToken(cur_str, &["<exception>"]));
+            q.push(ExprNode::Op(op));
+            Ok(())
+        };
+
+        let make_err_for_token = |last_token: Option<Token<'_>>, span: std::ops::Range<usize>| {
+            let expected: &[&str] = match last_token {
+                None | Some(Token::And) | Some(Token::Or) | Some(Token::OpenParen) => {
+                    &["<license>", "("]
+                }
+                Some(Token::CloseParen) => &["AND", "OR"],
+                Some(Token::Exception(_)) => &["AND", "OR", ")"],
+                Some(Token::License(LicenseItem::SPDX { .. })) => &["AND", "OR", "WITH", ")", "+"],
+                Some(Token::License(LicenseItem::Other { .. })) | Some(Token::Plus) => {
+                    &["AND", "OR", "WITH", ")"]
+                }
+                Some(Token::With) => &["<exception>"],
+            };
+
+            Err(ParseError {
+                original,
+                span,
+                reason: Reason::Unexpected(&expected),
+            })
+        };
+
+        // Basic implementation of the https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+        'outer: for tok in lexer {
+            let lt = tok?;
+            match &lt.token {
+                Token::License(item) => match last_token {
+                    None | Some(Token::And) | Some(Token::Or) | Some(Token::OpenParen) => {
+                        expr_queue.push(ExprNode::Req(LicenseReq {
+                            license: item.clone(),
+                            exception: None,
+                        }));
+                    }
+                    _ => return make_err_for_token(last_token, lt.span),
+                },
+                Token::Plus => match last_token {
+                    Some(Token::License(LicenseItem::SPDX { .. })) => {
+                        match expr_queue.last_mut().unwrap() {
+                            ExprNode::Req(LicenseReq {
+                                license: LicenseItem::SPDX { or_later, .. },
+                                ..
+                            }) => {
+                                *or_later = true;
+                            }
+                            _ => unreachable!(),
+                        }
+                    }
+                    _ => return make_err_for_token(last_token, lt.span),
+                },
+                Token::With => match last_token {
+                    Some(Token::License(_)) | Some(Token::Plus) => {}
+                    _ => return make_err_for_token(last_token, lt.span),
+                },
+                Token::Or | Token::And => match last_token {
+                    Some(Token::License(_))
+                    | Some(Token::CloseParen)
+                    | Some(Token::Exception(_))
+                    | Some(Token::Plus) => {
+                        let new_op = match lt.token {
+                            Token::Or => Op::Or,
+                            Token::And => Op::And,
+                            _ => unreachable!(),
+                        };
+
+                        while let Some(op) = op_stack.last() {
+                            match &op.op {
+                                Op::Open => break,
+                                top => {
+                                    if *top < new_op {
+                                        let top = op_stack.pop().unwrap();
+
+                                        match top.op {
+                                            Op::And | Op::Or => apply_op(top, &mut expr_queue)?,
+                                            _ => unreachable!(),
+                                        }
+                                    } else {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+
+                        op_stack.push(OpAndSpan {
+                            op: new_op,
+                            span: lt.span,
+                        });
+                    }
+                    _ => return make_err_for_token(last_token, lt.span),
+                },
+                Token::OpenParen => match last_token {
+                    None | Some(Token::And) | Some(Token::Or) | Some(Token::OpenParen) => {
+                        op_stack.push(OpAndSpan {
+                            op: Op::Open,
+                            span: lt.span,
+                        });
+                    }
+                    _ => return make_err_for_token(last_token, lt.span),
+                },
+                Token::CloseParen => {
+                    match last_token {
+                        Some(Token::License(_))
+                        | Some(Token::Plus)
+                        | Some(Token::Exception(_))
+                        | Some(Token::CloseParen) => {
+                            while let Some(top) = op_stack.pop() {
+                                match top.op {
+                                    Op::And | Op::Or => apply_op(top, &mut expr_queue)?,
+                                    Op::Open => {
+                                        // This is the only place we go back to the top of the outer loop,
+                                        // so make sure we correctly record this token
+                                        last_token = Some(Token::CloseParen);
+                                        continue 'outer;
+                                    }
+                                }
+                            }
+
+                            // We didn't have an opening parentheses if we get here
+                            return Err(ParseError {
+                                original,
+                                span: lt.span,
+                                reason: Reason::UnopenedParens,
+                            });
+                        }
+                        _ => return make_err_for_token(last_token, lt.span),
+                    }
+                }
+                Token::Exception(exc) => match last_token {
+                    Some(Token::With) => match expr_queue.last_mut() {
+                        Some(ExprNode::Req(lic)) => {
+                            lic.exception = Some(*exc);
+                        }
+                        _ => unreachable!(),
+                    },
+                    _ => return make_err_for_token(last_token, lt.span),
+                },
             }
 
-            expr_queue.push(ExprNode::Req(LicenseReq {
-                license: license_item,
-                exception: None,
-                or_later: false,
-            }));
+            last_token = Some(lt.token);
         }
 
-        while let Some(op) = op_stack.pop() {
-            match op {
-                Op::And => expr_queue.push(ExprNode::Op(Operator::And)),
-                Op::Or => expr_queue.push(ExprNode::Op(Operator::Or)),
-                Op::With => return Err(ParseError::UnexpectedToken("WITH", &["<exception>"])),
-                Op::Open(ind) => return Err(ParseError::UnbalancedParen(ind)),
+        // Validate that the terminating token is valid
+        match last_token {
+            Some(Token::License(_))
+            | Some(Token::Exception(_))
+            | Some(Token::CloseParen)
+            | Some(Token::Plus) => {}
+            // We have to have at least one valid license requirement
+            None => {
+                return Err(ParseError {
+                    original,
+                    span: 0..original.len(),
+                    reason: Reason::Empty,
+                });
+            }
+            _ => return make_err_for_token(last_token, original.len()..original.len()),
+        }
+
+        while let Some(top) = op_stack.pop() {
+            match top.op {
+                Op::And | Op::Or => apply_op(top, &mut expr_queue)?,
+                Op::Open => {
+                    return Err(ParseError {
+                        original,
+                        span: top.span,
+                        reason: Reason::UnclosedParens,
+                    });
+                }
             }
         }
-
-        // We have to have at least one valid license requirement
-        if !expr_queue.iter().any(|i| match i {
-            ExprNode::Req(_) => true,
-            _ => false,
-        }) {
-            return Err(ParseError::Empty);
-        }
-
-        // let trailing_parens = "Apache-2.0 WITH LLVM-exception OR Apache-2.0 AND (OpenSSL OR MIT)";
-        // Or => [Apache with LLVM, And => [Apache-2.0, Or => [OpenSSL, MIT]]]
-
-        // let leading_parens = "((Apache-2.0 WITH LLVM-exception) OR Apache-2.0) AND OpenSSL OR MIT";
-        // Or => [And => [Or => [Apache with LLVM, Apache-2.0], OpenSSL], MIT]
-
-        // let trailing_parens = "Apache-2.0 WITH LLVM-exception OR (Apache-2.0 AND OpenSSL OR MIT)";
-        // Or => [Apache with LLVM, And => [Apache-2.0, Or => [OpenSSL, MIT]]]
 
         Ok(ValidExpression {
+            original,
             expr: expr_queue,
-            original: expr,
         })
     }
 }

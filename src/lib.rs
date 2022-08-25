4001e1b60fb5df1243e8bece9cabe3deef207e3a
Jeremy Lempereur <jeremy.lempereur@iomentum.com>
2022-08-25 16:30:35 +0200

    Update license list to 3.18 (#55)
    * Update license list to 3.18
    
    * run clippy, cargo deny and cargo fmt
    
    * update the changelog, now that I know the PR#

--- src/lib.rs
+++ src/lib.rs
@@ -440,7 +440,6 @@ impl PartialEq for LicenseItem {
     }
 }
 
-#[must_use]
 impl fmt::Display for LicenseItem {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         match self {
@@ -542,7 +541,7 @@ pub fn exception_id(name: &str) -> Option<ExceptionId> {
 /// the license and exception identifiers are sourced from
 ///
 /// ```
-/// assert_eq!(spdx::license_version(), "3.14");
+/// assert_eq!(spdx::license_version(), "3.18");
 /// ```
 #[inline]
 #[must_use]

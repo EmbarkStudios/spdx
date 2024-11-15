ddc4afe9f253ac01a68cd61ab77fa76ec96751d0
Jake Shadle <jake.shadle@embark-studios.com>
2024-11-15 11:05:22 +0100

    Oops

--- src/lib.rs
+++ src/lib.rs
@@ -459,7 +459,7 @@ pub fn exception_id(name: &str) -> Option<ExceptionId> {
 /// the license and exception identifiers are sourced from
 ///
 /// ```
-/// assert_eq!(spdx::license_version(), "3.24.0");
+/// assert_eq!(spdx::license_version(), "3.25.0");
 /// ```
 #[inline]
 #[must_use]

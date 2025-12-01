645909da35ed0cc48c4ac62e1aa54feaa7551ec4
Jake Shadle <jake.shadle@embark-studios.com>
2025-12-01 15:20:50 +0100

    wtf github

--- src/detection/inline_cache.rs
+++ src/detection/inline_cache.rs
@@ -4,6 +4,7 @@ impl crate::detection::Store {
     /// Attempts to load the cached store inlined into this crate's source
     #[inline]
     pub fn load_inline() -> Result<Self, crate::detection::cache::CacheError> {
+        panic!("what in the actual fuck {:?}", std::str::from_utf8(CACHE));
         Self::from_cache(CACHE)
     }
 }

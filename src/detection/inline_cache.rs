a96b5f16902bf7866c60bcdccbd5b0b4fb0a02c1
Jake Shadle <jake.shadle@embark-studios.com>
2025-12-01 15:23:54 +0100

    Idiot

--- src/detection/inline_cache.rs
+++ src/detection/inline_cache.rs
@@ -4,7 +4,6 @@ impl crate::detection::Store {
     /// Attempts to load the cached store inlined into this crate's source
     #[inline]
     pub fn load_inline() -> Result<Self, crate::detection::cache::CacheError> {
-        panic!("what in the actual fuck {:?}", std::str::from_utf8(CACHE));
         Self::from_cache(CACHE)
     }
 }
